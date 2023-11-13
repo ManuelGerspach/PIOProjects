544d44ff6e28ed0f9e5a6deabff361d5bf29bd23
Manuel <40441375+Manuel2301@users.noreply.github.com>
2023-11-13 17:08:17 +0100

    cleanup

--- RangeComposition/categorySimplification.py
+++ RangeComposition/categorySimplification.py
@@ -1,3 +1,5 @@
+import statistics as s
+
 node_ID = "r:0:c" # can be copied in PIO solver if you right click on the board in the viewer
 position = "IP" # IP or OOP
 
@@ -13,23 +15,9 @@ def algorythm(connection):
     output = connection.command(line=f"load_tree {solution_path}")
     print_lines(output)
 
-    isComboInRange(connection, 1)
-
-    # read 1. category hands
-    readCat(connection, "0")
-        # for each category find out frequency of bet
-            # safe frequency in vairable and increase counter of all combos for this category
-                # safe frequency
-                    # print or/and safe average frequency for this single board
-
-    # TBD: erst eine category über alle boards oder alle categorien über ein board
-    # TBD: average über alle boards rechnen, nicht nur über ein board (nicht average frequencies zusammen rechnen, sondern average über alle boards bauen)
-
-    # read 2. category
-        # repeat step 1
-    # repeat for all categories
-
-    # repeat for all files in folder
+    # getFrequencies of a hand class and print the average of them
+    frequencies = getFrequenciesOfClass(connection, "4")
+    print(getAverage(frequencies))
 
 
 def readBetFrequency(connetion, index):
@@ -38,7 +26,7 @@ def readBetFrequency(connetion, index):
     comboFrequency = bettingStrat[index]
     return comboFrequency
 
-def readCat(connection, catNumer):
+def getFrequenciesOfClass(connection, catNumer):
     ## categories:
     # nothing 0, king_high 1, ace_high 2, low_pair 3, 3rd-pair 4, 2nd-pair 5, underpair 6,  top_pair 7, top_pair_tp8, overpair 9, two_pair 10, trips 11, set 12,  straight 13,  flush 14, fullhouse 15, top_fullhouse 16,  quads 17,  straight_flush19, 
     # no_draw 0,  4out_straight_draw 1, 8out_straight_draw 2,  flush_draw 3,  combo_draw 4
@@ -54,8 +42,17 @@ def readCat(connection, catNumer):
         if combo == catNumer and isComboInRange(connection, i):
             combosFrequencies.append(readBetFrequency(connection, i))
         i = i + 1
-    print(combosFrequencies)
+
     return combosFrequencies   
+
+def getAverage(listOfFrequencies):
+    # calc average
+    asFloat = []
+    for item in listOfFrequencies:
+        asFloat.append(float(item))
+    avg = sum(asFloat)/len(asFloat)
+    avg = round(avg, 2)
+    return avg
 
 
 def isComboInRange(connection, index):
